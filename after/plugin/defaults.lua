c85eb4d2512ebb0310552a48b75b0b78a3acc264
Rafael Veronezi <ravero@outlook.com>
2023-05-27 16:48:15 -0300

    Changed yank-on-delete and clipboard behavior.

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,2 +1,14 @@
 vim.opt.relativenumber = true
+vim.opt.clipboard = 'unnamedplus'
+
+-- Remap Yank-on-delete
+vim.keymap.set({ 'n' }, 'x', '"_x')
+vim.keymap.set({ 'n' }, 'X', '"_X')
+vim.keymap.set({ 'n' }, '<Del>', '"_x')
+vim.keymap.set({ 'n' }, 'd', '"_d')
+vim.keymap.set({ 'n' }, 'D', '"_D')
+
+-- Window Navigation Keymaps
+vim.keymap.set({ 'n', 'v' }, '<C-l>', '<C-w>l')
+vim.keymap.set({ 'n', 'v' }, '<C-h>', '<C-w>h')
 
